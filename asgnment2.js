6ac72928dd5d43477a8f2640089a2e965a020d57
christopherccr <91065367+christopherccr@users.noreply.github.com>
2022-10-17 15:49:09 +1000

    Add files via upload

--- asgnment2.js
+++ asgnment2.js
@@ -15,3 +15,9 @@ vegaEmbed("#combine_chart", vg_3).then(function(result) {
 // Access the Vega view instance
 //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_4 = "pie.json";
+vegaEmbed("#pie_chart", vg_4).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
